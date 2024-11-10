fe9ba437f411ddfaa8bc17158907e8434fec91ed
Adhe <adhepluk@gmail.com>
2024-11-11 01:07:10 +0700

    Disable barbar animation

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -3,14 +3,14 @@
 return {
   'romgrk/barbar.nvim',
   dependencies = {
-    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    { 'nvim-tree/nvim-web-devicons', enabled = true },
   },
   init = function()
     vim.g.barbar_auto_setup = false
   end,
   opts = {
     -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-    -- animation = true,
+    animation = false,
     -- insert_at_start = true,
     icons = {
       filetype = {
@@ -18,4 +18,5 @@ return {
       },
     },
   },
+  version = '^1.0.0',
 }
